0e77c3a493387f9fd893ba0c06428c438a1dd74a
João Paulo Borges <joaopaulo.borges.gm@hotmail.com>
2022-06-01 12:43:04 -0300

    adding docker-compose and nginx

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -2,11 +2,15 @@
 import * as dotenv from 'dotenv';
 
 export function bootstrapEnvironmentVariables() {
-  let path = '.env';
-  if (process.env.NODE_ENV === 'test') {
-    path = '.env.test';
+  console.log(`Initializing in ${process.env.NODE_ENV ?? 'development'} mode`);
+
+  if (process.env.NODE_ENV === 'production') {
+    return;
   }
+
+  const isTest = process.env.NODE_ENV === 'test';
+  const path = isTest ? '.env.test' : '.env';
+
   const { parsed } = dotenv.config({ path });
   for (const key in parsed) process.env[key] = parsed[key];
-  console.log(`Initializing in ${process.env.NODE_ENV ?? 'development'} mode`);
 }
